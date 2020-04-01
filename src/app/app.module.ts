e7bfe5fa075836fb763aaeba9ffef9959b2beedb
mitr0fan <51967345+mitr0fan@users.noreply.github.com>
2020-04-01 17:46:17 +0400

    added templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app-component/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { ChannelsComponent } from './components/channels/channels.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HeaderComponent, MoviesComponent, MovieItemComponent, ChannelsComponent],
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
